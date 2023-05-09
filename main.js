d0df3984f6981e986a20752a9d4d608655476e38
Daniela Wellisz <daniela.wellisz@gmail.com>
2023-05-09 13:18:51 -0700

    add some stitch styling

--- main.js
+++ main.js
@@ -7,26 +7,33 @@ for (var i = 0; i <= 500; i++) {
     }
 document.querySelector('#app').innerHTML = `
 <p>Adjust the variables in the SCSS in order to preview what your planned pooling pattern could look like with different row lengths</p>
-<div class="containerA" id="pooler1">
+<div class="moss" id="pooler1">
 ${manyDivs}
 </div>
-<div class="containerB" id="pooler2">
+<div class="stacked" id="pooler2">
+${manyDivs}
+</div>
+<div class="granny" id="pooler3">
+${manyDivs}
+</div>
+<div class="v-stitch" id="pooler4">
+${manyDivs}
+</div>
+<div class="shell" id="pooler5">
 ${manyDivs}
 </div>
 `
 
-paintStitches(document.querySelector('#pooler1'), [
+const colorConfig = [
   {color: "#C274B3", length: 2},
   {color: "#4ECDC4", length: 3},
   {color: "#FFE66D", length: 1},
   {color: "#FF6B6B", length: 2},
-  {color: "#F7FFF7", length: 13},
-])
+  {color: "#EEEEEE", length: 13},
+]
 
-paintStitches(document.querySelector('#pooler2'), [
-  {color: "#C274B3", length: 2},
-  {color: "#4ECDC4", length: 3},
-  {color: "#FFE66D", length: 1},
-  {color: "#FF6B6B", length: 2},
-  {color: "#F7FFF7", length: 13},
-])
+paintStitches(document.querySelector('#pooler1'), colorConfig)
+paintStitches(document.querySelector('#pooler2'), colorConfig)
+paintStitches(document.querySelector('#pooler3'), colorConfig)
+paintStitches(document.querySelector('#pooler4'), colorConfig)
+paintStitches(document.querySelector('#pooler5'), colorConfig)
