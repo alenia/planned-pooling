4d4d0ffe22457c4e212002e1e1cf6bf8c87319aa
Daniela Wellisz <daniela.wellisz@gmail.com>
2023-05-11 23:21:47 -0700

    clean up loops using repeat method

--- swatch.js
+++ swatch.js
@@ -2,24 +2,17 @@ import { paintStitches } from './color.js'
 const clusterMap = { jasmine: 3}
 export function buildSwatch(swatch, { colorConfig, crowLength, stitchPattern, crows = 40 }) {
   let cont = ""
-  for (var i = 0; i < crows; i++) {
-    cont += ('<div class="crow">');
-    const cluster = clusterMap[stitchPattern];
-    if(cluster) {
+  const clusterLength = clusterMap[stitchPattern];
+  if(clusterLength) {
+    for (var i = 0; i < crows; i++) {
+      cont += ('<div class="crow">');
       cont += '<div class="cluster"><div class="stitch"></div></div>'; //TODO: this is just for Jasmine
-      for (var j = 0; j < crowLength; j++ ) {
-        cont += '<div class="cluster">';
-        for (var k = 0; k < cluster; k++ ) {
-          cont += (`<div class="stitch"></div>`);
-        }
-        cont += '</div>';
-      }
-    } else {
-      for (var j = 0; j < crowLength; j++ ) {
-        cont += (`<div class="stitch"></div>`);
-      }
+      cont += `<div class="cluster">${('<div class="stitch"></div>').repeat(clusterLength)}</div>`.repeat(crowLength);
+      cont += ('</div>');
     }
-    cont += ('</div>');
+  }
+  else {
+    cont = `<div class="crow">${(`<div class="stitch"></div>`).repeat(crowLength)}</div>`.repeat(crows)
   }
 
   swatch.classList.add('swatch');
