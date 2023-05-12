9e941714360e76e56a10a463f25eae8c42070df3
Daniela Wellisz <daniela.wellisz@gmail.com>
2023-05-11 21:46:23 -0700

    got rid of stupid variable name

--- swatch.js
+++ swatch.js
@@ -1,29 +1,29 @@
 import { paintStitches } from './color.js'
 const clusterMap = { jasmine: 3}
-export function buildSwatch(swatch, { colorConfig, stitchesPerCrow, stitchPattern, crows = 40 }) {
-  let crowsOfStitches = ""
+export function buildSwatch(swatch, { colorConfig, crowLength, stitchPattern, crows = 40 }) {
+  let cont = ""
   for (var i = 0; i < crows; i++) {
-    crowsOfStitches += ('<div class="crow">');
+    cont += ('<div class="crow">');
     const cluster = clusterMap[stitchPattern];
     if(cluster) {
-      crowsOfStitches += '<div class="cluster"><div class="stitch"></div></div>'; //TODO: this is just for Jasmine
-      for (var j = 0; j < stitchesPerCrow; j++ ) {
-        crowsOfStitches += '<div class="cluster">';
+      cont += '<div class="cluster"><div class="stitch"></div></div>'; //TODO: this is just for Jasmine
+      for (var j = 0; j < crowLength; j++ ) {
+        cont += '<div class="cluster">';
         for (var k = 0; k < cluster; k++ ) {
-          crowsOfStitches += (`<div class="stitch"></div>`);
+          cont += (`<div class="stitch"></div>`);
         }
-        crowsOfStitches += '</div>';
+        cont += '</div>';
       }
     } else {
-      for (var j = 0; j < stitchesPerCrow; j++ ) {
-        crowsOfStitches += (`<div class="stitch"></div>`);
+      for (var j = 0; j < crowLength; j++ ) {
+        cont += (`<div class="stitch"></div>`);
       }
     }
-    crowsOfStitches += ('</div>');
+    cont += ('</div>');
   }
 
   swatch.classList.add('swatch');
   swatch.classList.add(stitchPattern);
-  swatch.innerHTML = crowsOfStitches;
+  swatch.innerHTML = cont;
   paintStitches(swatch, colorConfig);
 }
