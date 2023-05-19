13c9fc5629699b5ac4c3b50494098b42c8fda0bd
Daniela Wellisz <daniela.wellisz@gmail.com>
2023-05-19 10:49:54 -0700

    sunflower swatch stuff

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.scss'
 import { buildSwatch } from './swatch.js'
 
-document.querySelector('#app').innerHTML = `
+let old = `
 <p>Adjust the variables in order to preview what your planned pooling pattern could look like with different row lengths</p>
 <div id="sc-hat"></div>
 <div id="shell-hat"></div>
@@ -21,25 +21,57 @@ document.querySelector('#app').innerHTML = `
 <div id="angled-block-cluster"></div>
 <div id="angled-block-cluster2" class="vertical"></div>
 `
+document.querySelector('#app').innerHTML = `
+<p>Here's my chart</p>
+<div id="angled-block-cluster" class="vertical"></div>
+<div id="angled-block-cluster2" class="vertical"></div>
+<p>Here's how it works overlapped</p>
+<div id="angled-block-cluster3" class="vertical overlapping"></div>
+<div id="angled-block-cluster4" class="vertical overlapping swap"></div>
+<div id="angled-block-cluster5" class="vertical overlapping"></div>
+<div id="angled-block-cluster6" class="vertical overlapping swap"></div>
+`
 
-const orange = "#fcd67c";
+const lightOrange = "#fcd67c";
 const cream = "#fcf7eb";
 const blue = "#cff7fc";
 const colorConfigHatSC = [
-  {color: orange, length: 5},
+  {color: lightOrange, length: 5},
   {color: cream, length: 4},
-  {color: orange, length: 5},
+  {color: lightOrange, length: 5},
   {color: blue, length: 5},
   {color: cream, length: 4},
   {color: blue, length: 5},
 ]
 const colorConfigHatShell = [
-  {color: orange, length: 1},
+  {color: lightOrange, length: 1},
   {color: cream, length: 1},
-  {color: orange, length: 1},
+  {color: lightOrange, length: 1},
   {color: blue, length: 1},
   {color: cream, length: 1},
   {color: blue, length: 1},
+]
+
+const purple = "#C274B3";
+const teal = "#4ECDC4";
+const brown = "#967527";
+const orange = "#f0ab0a";
+//const colorConfigHeatheredDC = [
+  //{ color: purple, length: 5 },
+  //{ color: teal, length: 2 },
+  //{ color: orange, length: 2 }, 
+  //{ color: purple, length: 2 },
+  //{ color: brown, length: 3 }, 
+  //{ color: orange, length: 1 }, 
+  //{ color: teal, length: 2 },
+//]
+const colorConfigHeatheredDC = [
+  { color: purple, length: 5 },
+  { color: teal, length: 2 },
+  { color: orange, length: 2 }, 
+  { color: brown, length: 5 }, 
+  { color: orange, length: 1 }, 
+  { color: teal, length: 2 },
 ]
 
 
@@ -82,5 +114,12 @@ buildSwatch(document.querySelector('#ripple-pooler'), { colorConfig, crowLength:
 buildSwatch(document.querySelector('#ripple-pooler2'), { colorConfig, crowLength: 4, stitchPattern: 'ripple' })
 buildSwatch(document.querySelector('#ripple-pooler3'), { colorConfig, crowLength: 4, stitchPattern: 'ripple' })
 buildSwatch(document.querySelector('#vstitch-cluster'), { colorConfig: colorConfigScarf2, crowLength: 12, stitchPattern: 'vstitchCluster' })
-buildSwatch(document.querySelector('#angled-block-cluster'), { colorConfig, crowLength: 3, stitchPattern: 'ablockCluster' })
-buildSwatch(document.querySelector('#angled-block-cluster2'), { colorConfig, crowLength: 3, stitchPattern: 'ablockCluster' })
+//buildSwatch(document.querySelector('#angled-block-cluster'), { colorConfig: colorConfigHeatheredDC, crows: 9, crowLength: 8, stitchPattern: 'ablockCluster' })
+buildSwatch(document.querySelector('#angled-block-cluster'), { colorConfig: colorConfigHeatheredDC, crows: 2, crowLength: 17, stitchPattern: 'ablockCluster', colorShift: 10 })
+buildSwatch(document.querySelector('#angled-block-cluster2'), { colorConfig: colorConfigHeatheredDC, crows: 2, crowLength: 17, stitchPattern: 'ablockCluster', colorShift: 2 })
+//buildSwatch(document.querySelector('#angled-block-cluster2'), { colorConfig: colorConfigHeatheredDC, crowLength: 8, stitchPattern: 'ablockCluster' })
+buildSwatch(document.querySelector('#angled-block-cluster3'), { colorConfig: colorConfigHeatheredDC, crows: 5, crowLength: 17, stitchPattern: 'ablockCluster', colorShift: 2 })
+buildSwatch(document.querySelector('#angled-block-cluster4'), { colorConfig: colorConfigHeatheredDC, crows: 5, crowLength: 17, stitchPattern: 'ablockCluster', colorShift: 10 })
+buildSwatch(document.querySelector('#angled-block-cluster5'), { colorConfig: colorConfigHeatheredDC, crows: 5, crowLength: 17, stitchPattern: 'ablockCluster', colorShift: 2 })
+buildSwatch(document.querySelector('#angled-block-cluster6'), { colorConfig: colorConfigHeatheredDC, crows: 5, crowLength: 17, stitchPattern: 'ablockCluster', colorShift: 10 })
+//buildSwatch(document.querySelector('#angled-block-cluster2'), { colorConfig: colorConfigHeatheredDC, crowLength: 25, stitchPattern: 'ablockCluster' })
