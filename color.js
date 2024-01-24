60aea74d3deb498df186d14c2a8b6ef24e1a24c4
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-01-24 14:25:13 -0800

    remove unused method

--- color.js
+++ color.js
@@ -1,10 +1,3 @@
-export function paintStitches(pooler, colorConfig, { colorShift } = { colorShift: 0 } ) {
-  let colorSequence = colorConfig.reduce((ary, conf) => ary.concat(new Array(conf.length).fill(conf.color)), []);
-  Array.from(pooler.querySelectorAll('.stitch')).forEach((stitch, i) => {
-    stitch.style.backgroundColor = colorSequence[(i + colorShift) % colorSequence.length];
-  })
-}
-
 export function nextStitchColorByIndex(i, colorConfig, { colorShift } = { colorShift: 0 } ) {
   let colorSequence = colorConfig.reduce((ary, conf) => ary.concat(new Array(conf.length).fill(conf.color)), []);
   return colorSequence[(i + colorShift) % colorSequence.length];
