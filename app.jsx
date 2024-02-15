d12c11f3c4b473cccfaeea9fc1a8da1db304663d
Joan Wolkerstorfer <joanwolk@gmail.com>
2024-02-14 16:13:30 -0800

    add form to change stitch count - add React <StrictMode> to help development debugging - add Form - remove Preview from rendering

---
+++ app.jsx
@@ -0,0 +1,49 @@
+// import "./App.css";
+import Swatch, { buildSwatch } from './swatch.jsx';
+import Preview from './preview.jsx';
+import Form from './form.jsx';
+import React, { useState } from "react";
+
+const orange = "#fcd67c";
+const cream = "#fcf7eb";
+const blue = "#cff7fc";
+
+function App() {
+  const [formData, setFormData] = useState({
+    colorConfig: [
+      {color: orange, length: 4},
+      {color: cream, length: 3},
+      {color: orange, length: 4},
+      {color: blue, length: 3},
+      {color: cream, length: 3},
+      {color: blue, length: 3},
+    ],
+    crowLength: 39,
+    crows: 12,
+    colorShift: 6,
+    staggerLengths: false,
+    stitchPattern: 'moss',
+  })
+
+  return (
+  <div className="container">
+    <p>Adjust the variables in order to preview what your planned pooling pattern could look like with different row lengths</p>
+    <Form
+      formData={formData}
+      setFormData={setFormData} 
+    />
+    <Swatch
+      id="hdc-hat"
+      className="vertical"
+      colorConfig={formData.colorConfig}
+      crowLength={formData.crowLength}
+      crows={formData.crows}
+      colorShift={formData.colorShift}
+      staggerLengths={formData.staggerLengths}
+      stitchPattern={formData.stitchPattern}
+    />
+  </div>
+  );
+}
+
+export default App;
---
+++ form.jsx
@@ -0,0 +1,75 @@
+const Form = ({ formData, setFormData }) => {
+  const { colorConfig, crowLength, crows, colorShift, staggerLengths, stitchPattern } = formData;
+
+  const onChange = (e) => {
+    const newFormData = { ...formData };
+    newFormData[e.target.name] = e.target.value;
+    setFormData(newFormData);
+  };
+
+  const onChangeNumber = (e) => {
+    const newFormData = { ...formData };
+    newFormData[e.target.name] = parseInt( e.target.value );
+    setFormData(newFormData);
+  };
+
+  const onSubmit = () => {
+    // debugger
+    console.log("formData:")
+    console.log(formData)
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit();
+      }}
+    >
+      <label htmlFor="crowLength" title="The number of stitches in one row">
+        Stitches per row:
+      </label>
+      <input
+        onChange={onChangeNumber}
+        type="number"
+        inputMode="numeric"
+        pattern="[0-9]*"
+        name="crowLength"
+        id="crowLength"
+        value={crowLength}
+      />
+
+      <input
+        type="hidden"
+        name="crows"
+        id="crows"
+        value={crows}
+      />
+
+      <input
+        type="hidden"
+        name="colorShift"
+        id="colorShift"
+        value={colorShift}
+      />
+
+      <input
+        type="hidden"
+        name="staggerLengths"
+        id="staggerLengths"
+        value={staggerLengths}
+      />
+
+      <input
+        type="hidden"
+        name="stitchPattern"
+        id="stitchPattern"
+        value={stitchPattern}
+      />
+
+      <input type="submit" value="See it" className="button" />
+    </form>
+  );
+}
+
+export default Form;
