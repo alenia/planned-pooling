7f0648a51920182eb4e6fcf21a681d9110d12989
Joan Wolkerstorfer <joanwolk@gmail.com>
2024-02-22 11:39:11 -0800

    display total color sequence length; fixes https://github.com/alenia/planned-pooling/issues/19

--- src/Form.jsx
+++ src/Form.jsx
@@ -38,6 +38,22 @@ const Form = ({ formData, setFormData }) => {
     return result;
   }
 
+  const printColorSequenceLength = () => {
+    let result = 0;
+    for (const i in colorConfig) {
+      result += parseInt(colorConfig[i].length);
+    }
+    return result;
+  }
+
+  const printColorSequenceLength = () => {
+    let result = 0;
+    for (const i in colorConfig) {
+      result += parseInt(colorConfig[i].length);
+    }
+    return result;
+  }
+
   return (
     <form
       onSubmit={(e) => {
@@ -45,7 +61,13 @@ const Form = ({ formData, setFormData }) => {
       }}
     >
       <div>
-        {printColorConfig()}
+        <p>{printColorConfig()}</p>
+      </div>
+
+      <div>
+        <p>
+          Total stitches in color sequence: {printColorSequenceLength()}
+        </p>
       </div>
 
       <div>
