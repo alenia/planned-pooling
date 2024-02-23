87a134e5cda60b56c22aa5e27ccdb553d1e53841
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-02-22 17:04:58 -0800

    add basic routing that renders different components
    fixes #16

--- src/wrapper.jsx
+++ src/wrapper.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Preview from './projects/Preview.jsx'
+import Sunflower from './projects/Sunflower.jsx'
+
 //import './index.css'
+
+const router = () => {
+  switch (window.location.pathname) {
+    case "/preview":
+      return Preview;
+    case "/sunflower":
+      return Sunflower;
+    default:
+    return App;
+  }
+}
+
+const Main = router();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
 )
