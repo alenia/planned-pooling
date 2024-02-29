7d526f60409acc08e74363b89fb1ab9557b9a53e
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-02-28 17:28:06 -0800

    add spinner buttons on number inputs

--- src/inputs/Integer.jsx
+++ src/inputs/Integer.jsx
@@ -1,25 +1,40 @@
 import PropTypes from "prop-types";
 
 const IntegerInput = ({ value, name, label, title, setValue, validator = () => true}) => {
+  const handleButtonChange = (direction) => {
+    let v = value;
+    if(direction === "down") {
+      v -= 1
+    }
+    if(direction === "up") {
+      v += 1
+    }
+    if(!validator(v)) { return }
+    setValue(name, v)
+  }
   const handleChange = (e) => {
-    let value = parseInt(e.target.value)
-    if (isNaN(value) || !validator(value)) { value = 0 }
-    setValue(e.target.name, value);
+    let v = parseInt(e.target.value)
+    if (isNaN(value) || !validator(v)) { v = 0 }
+    setValue(e.target.name, v);
   }
   return (
-      <div>
+      <div className="input-group integer-input">
         <label htmlFor={name} title={title}>
           {label}
         </label>
-        <input
-          type="number"
-          inputMode="numeric"
-          pattern="[0-9]*"
-          onChange={handleChange}
-          name={name}
-          id={name}
-          value={value}
-        />
+        <div className="number-spinner">
+          <button onClick={() => handleButtonChange("down")}>-</button>
+          <input
+            type="number"
+            inputMode="numeric"
+            pattern="[0-9]*"
+            onChange={handleChange}
+            name={name}
+            id={name}
+            value={value}
+          />
+          <button onClick={() => handleButtonChange("up")}>+</button>
+        </div>
       </div>
   )
 };
