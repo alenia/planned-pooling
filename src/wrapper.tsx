546ff254a01b6d03f15fc51e0e813d9df64140a1
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-03-05 20:58:21 -0800

    typescript the wrapper and clean up

--- src/wrapper.tsx
+++ src/wrapper.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Preview from './projects/Preview.tsx'
-import Sunflower from './projects/Sunflower.tsx'
-import Doodle from './projects/Doodle.tsx'
+import App from './App'
+import Preview from './projects/Preview'
+import Sunflower from './projects/Sunflower'
+import Doodle from './projects/Doodle'
 import './index.scss'
 
 const router = () => {
@@ -21,7 +21,7 @@ const router = () => {
 
 const Main = router();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Main />
   </React.StrictMode>,
