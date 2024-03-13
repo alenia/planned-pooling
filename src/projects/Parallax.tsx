8d08784e47d948933882b26622712542c4c1907f
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-03-13 11:53:13 -0700

    add parallax endpoint for beanie pattern

---
+++ src/projects/Parallax.tsx
@@ -0,0 +1,28 @@
+import SwatchWithForm from '../SwatchWithForm';
+import { StitchPattern, ColorSequenceArray } from '../types'
+import { Fragment, useState } from "react";
+
+function Parallax() {
+  const [swatchConfig, setSwatchConfig] = useState({
+    colorSequence: [
+      {color: "#E0962B", length: 3},
+      {color: "#157255", length: 16},
+    ] as ColorSequenceArray,
+    stitchesPerRow: 29,
+    numberOfRows: 38,
+    colorShift: 0,
+    staggerLengths: false,
+    stitchPattern: StitchPattern.unstyled,
+    showRowNumbers: true,
+  })
+
+  return (
+    <Fragment>
+      <p>This is a page to help with the parallax beanie chart. If you want to design more planned pooling patterns, I recommend the <a href='/'>main app.</a></p>
+      <SwatchWithForm swatchConfig={swatchConfig} setSwatchConfig={setSwatchConfig} />
+    </Fragment>
+  );
+}
+
+export default Parallax;
+
