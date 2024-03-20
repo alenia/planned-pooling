9a28aeaf9e006a7830cd95aa36bf0805ce8c762a
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-03-20 14:19:27 -0700

    add dropdown input and change colorways file to be typescript

--- src/colorways.ts
+++ src/colorways.ts
@@ -1,17 +1,26 @@
-[
-  {
+import { ColorInSequence } from './types'
+type Colorway = {
+  yarnName: string,
+  colorway: string,
+  id: string,
+  colorSequence: Array<ColorInSequence>
+}
+
+export const dunaColorways : Array<Colorway> = [ {
     "yarnName": "Circulo Duna",
     "colorway": "9391 Aloe Vera",
+    "id": "duna-9391",
     "colorSequence": [
-      {"color": "#a6c65f", "length": 3},
-      {"color": "#486111", "length": 5},
-      {"color": "#a6c65f", "length": 3},
-      {"color": "#dae2c9", "length": 5}
+      {"color": "#99c944", "length": 3},
+      {"color": "#325303", "length": 5},
+      {"color": "#99c944", "length": 3},
+      {"color": "#ecf5da", "length": 5}
     ]
   },
   {
     "yarnName": "Circulo Duna",
     "colorway": "9642 Intuition",
+    "id": "duna-9642",
     "colorSequence": [
       {"color": "#0e0e66", "length": 3},
       {"color": "#ff001d", "length": 3},
@@ -24,6 +33,7 @@
   {
     "yarnName": "Circulo Duna",
     "colorway": "9482 Pacific",
+    "id": "duna-9482",
     "colorSequence": [
       {"color": "#1C40B8", "length": 4},
       {"color": "#121253", "length": 5},
@@ -34,6 +44,7 @@
   {
     "yarnName": "Circulo Duna",
     "colorway": "9128 Winter",
+    "id": "duna-9128",
     "colorSequence": [
       {"color": "#3562d5", "length": 5},
       {"color": "#73BEF4", "length": 3},
@@ -44,6 +55,7 @@
   {
     "yarnName": "Circulo Duna",
     "colorway": "9687 Caravel",
+    "id": "duna-9687",
     "colorSequence": [
       {"color": "#542E0F", "length": 5},
       {"color": "#BC9371", "length": 3},
@@ -54,6 +66,7 @@
   {
     "yarnName": "Circulo Duna",
     "colorway": "9165 Yellow Hibiscus",
+    "id": "duna-9165",
     "colorSequence": [
       {"color": "#f51505", "length": 5},
       {"color": "#f57605", "length": 3},
@@ -64,6 +77,7 @@
   {
     "yarnName": "Circulo Duna",
     "colorway": "9492 Sunflower",
+    "id": "duna-9492",
     "colorSequence": [
       {"color": "#542E0F", "length": 5},
       {"color": "#c28a11", "length": 3},
@@ -72,3 +86,7 @@
     ]
   }
 ]
+
+const colorways : Array<Colorway> = { ...dunaColorways }
+
+export default colorways;
