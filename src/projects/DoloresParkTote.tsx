87a1a28a9cc84bfea2fe159cf6dba7faabb1fdc5
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-03-29 13:46:44 -0700

    Add in buttons to reset the width of the swatch based on your panel in the pattern

--- src/projects/DoloresParkTote.tsx
+++ src/projects/DoloresParkTote.tsx
@@ -36,7 +36,29 @@ function DoloresParkTote() {
       ...swatchConfig,
       colorSequence: newColorSequence,
       stitchesPerRow: totalColorSequenceLength(newColorSequence),
-      colorShift: 0
+      colorShift: 0,
+    })
+  }
+  const setPanel1Configuration = () => {
+    setSwatchConfig({
+      ...swatchConfig,
+      stitchesPerRow: totalColorSequenceLength(swatchConfig.colorSequence),
+      staggerLengths: false,
+
+    })
+  }
+  const setPanel2Configuration = () => {
+    setSwatchConfig({
+      ...swatchConfig,
+      stitchesPerRow: totalColorSequenceLength(swatchConfig.colorSequence),
+      staggerLengths: true,
+    })
+  }
+  const setBandConfiguration = () => {
+    setSwatchConfig({
+      ...swatchConfig,
+      stitchesPerRow: Math.floor(totalColorSequenceLength(swatchConfig.colorSequence)/2),
+      staggerLengths: false,
     })
   }
 
@@ -47,16 +69,26 @@ function DoloresParkTote() {
       <p> For Panel 2: Select <em>Stretch Colors</em> to preview the color stretching technique</p>
       <p> For the band: Turn off Stretch Colors and set <em>Stitches Per Row</em> to half of your color sequence</p>
       <p> You can play with the color shift to change where you start your band or Panel 1</p>
-      <DropdownInput
-        label="Pick a colorway"
-        name="colorway"
-        title="Pick from a Circulo DUNA colorway"
-        value={selectedColorway}
-        setValue={resetColorway}
-        items={[...Object.keys(dunaColorways).map((id) => (
-          { label: dunaColorways[id].colorway, value: id }
-        )), {label: 'Custom (choose your own colors)', value: 'custom'}]}
-      />
+      <fieldset>
+        <DropdownInput
+          label="Pick a colorway"
+          name="colorway"
+          title="Pick from a Circulo DUNA colorway"
+          value={selectedColorway}
+          setValue={resetColorway}
+          items={[...Object.keys(dunaColorways).map((id) => (
+            { label: dunaColorways[id].colorway, value: id }
+          )), {label: 'Custom (choose your own colors)', value: 'custom'}]}
+        />
+        <label>
+          Set the stitches per row and color stretching based on your panel:
+        </label>
+        <div className="color-buttons">
+          <button type="button" onClick={setPanel1Configuration}>Panel 1 (stripes)</button>
+          <button type="button" onClick={setPanel2Configuration}>Panel 2 (color stretching)</button>
+          <button type="button" onClick={setBandConfiguration}>Band (half width)</button>
+        </div>
+      </fieldset>
       <SwatchWithForm
         swatchConfig={swatchConfig}
         setSwatchConfig={setSwatchConfig}
