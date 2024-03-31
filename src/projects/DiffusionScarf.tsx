92a761695140902dbaa1e4544a7473192b615b2a
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-03-31 10:10:21 -0700

    display specific chart for each swatch in the scarf

--- src/projects/DiffusionScarf.tsx
+++ src/projects/DiffusionScarf.tsx
@@ -2,6 +2,7 @@ import Swatch from '../Swatch';
 import { StitchPattern, Color, ColorSequenceArray } from '../types'
 import IntegerInput from '../inputs/Integer'
 import { useState } from "react";
+import { totalColorSequenceLength } from '../color'
 
 function DiffusionScarf() {
   const [mainColorShift, setMainColorShift] = useState(0)
@@ -29,7 +30,6 @@ function DiffusionScarf() {
     numberOfRows: 30,
     colorShift: mainColorShift,
     staggerLengths: false,
-    stitchPattern: StitchPattern.compactMoss,
   }
 
   const neonPurple = '#e100ff' as Color
@@ -39,21 +39,36 @@ function DiffusionScarf() {
   const neonGreen = '#79FF04' as Color
   const neonBlue = '#04FFFF' as Color
 
+  const panelConfigs = [
+    { stitchesPerRow: 10, colorShift: 7, colorSequence: colorSequenceWithAccent(neonOrange) },
+    { stitchesPerRow: 20, colorShift: 11, colorSequence: stretchedColorSequenceWithAccent(neonPink) },
+    { stitchesPerRow: 19, colorShift: 4, colorSequence: colorSequenceWithAccent(neonPurple) },
+    { stitchesPerRow: 18, colorShift: 13, colorSequence: colorSequenceWithAccent(neonBlue) },
+    { stitchesPerRow: 16, colorShift: 1, colorSequence: colorSequenceWithAccent(neonGreen) },
+    { stitchesPerRow: 16, colorShift: 3, colorSequence: oddColorSequenceWithAccent(neonYellow) },
+    { stitchesPerRow: 15, colorShift: 17, colorSequence: colorSequenceWithAccent(neonGreen) },
+    { stitchesPerRow: 14, colorShift: 5,  colorSequence: oddColorSequenceWithAccent(neonBlue) },
+    { stitchesPerRow: 12, colorShift: 5,  colorSequence: colorSequenceWithAccent(neonPurple) },
+    { stitchesPerRow: 12, colorShift: 13, colorSequence: oddColorSequenceWithAccent(neonPink) },
+    { stitchesPerRow: 11, colorShift: 3,  colorSequence: colorSequenceWithAccent(neonOrange) },
+    { stitchesPerRow: 11, colorShift: 7,  colorSequence: oddColorSequenceWithAccent(neonYellow) },
+  ]
+
   return (
     <div className="container">
       <div className="squashed-swatch-container">
-        <Swatch {...sharedConfig} stitchesPerRow={10} colorShift={7} colorSequence={colorSequenceWithAccent(neonOrange)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={20} colorShift={11} colorSequence={stretchedColorSequenceWithAccent(neonPink)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={19} colorShift={4} colorSequence={colorSequenceWithAccent(neonPurple)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={18} colorShift={13} colorSequence={colorSequenceWithAccent(neonBlue)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={16} colorShift={1} colorSequence={colorSequenceWithAccent(neonGreen)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={16} colorShift={3} colorSequence={oddColorSequenceWithAccent(neonYellow)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={15} colorShift={17} colorSequence={colorSequenceWithAccent(neonGreen)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={14} colorShift={5} colorSequence={oddColorSequenceWithAccent(neonBlue)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={12} colorShift={5} colorSequence={colorSequenceWithAccent(neonPurple)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={12} colorShift={13} colorSequence={oddColorSequenceWithAccent(neonPink)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={11} colorShift={3} colorSequence={colorSequenceWithAccent(neonOrange)}/>
-        <Swatch {...sharedConfig} stitchesPerRow={11} colorShift={7} colorSequence={oddColorSequenceWithAccent(neonYellow)}/>
+        {panelConfigs.map((specificConfig) => <Swatch {...sharedConfig} stitchPattern={StitchPattern.compactMoss} {...specificConfig} />)}
+      </div>
+      <div className="container">
+        {panelConfigs.map((specificConfig) => (
+          <div>
+            <pre>
+              stitches per row: {specificConfig.stitchesPerRow}<br/>
+              color sequence length: {totalColorSequenceLength(specificConfig.colorSequence)}
+            </pre>
+            <Swatch className='numbered' {...sharedConfig} stitchPattern={StitchPattern.moss} {...specificConfig} />
+          </div>
+          ))}
       </div>
     </div>
   );
