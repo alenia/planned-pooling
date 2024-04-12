2373fdb2d852803a62734ba0e14f1648c4386c0b
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-04-11 21:40:29 -0700

    experimental endpoint now shows component named Experimental, add a few existing features to this page

--- src/projects/Experimental.test.tsx
+++ src/projects/Experimental.test.tsx
@@ -1,13 +1,13 @@
 import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from "react-router-dom";
-import StretchLengths from './StretchLengths'
+import Experimental from './Experimental'
 
-describe('StretchLengths', () => {
-  it('renders the StretchLengths component with a swatch in it', () => {
+describe('Experimental', () => {
+  it('renders the Experimental component with a swatch in it', () => {
     render(
       <MemoryRouter >
-        <StretchLengths />
+        <Experimental />
       </MemoryRouter>
     )
     expect(screen.getAllByTestId("swatch").length).toEqual(1)
--- src/wrapper.tsx
+++ src/wrapper.tsx
@@ -6,7 +6,7 @@ import Preview from './projects/Preview'
 import Sunflower from './projects/Sunflower'
 import Doodle from './projects/Doodle'
 import LogoOption from './projects/LogoOption'
-import StretchLengths from './projects/StretchLengths'
+import Experimental from './projects/Experimental'
 import Parallax from './projects/Parallax'
 import DoloresParkTote from './projects/DoloresParkTote'
 import DiffusionScarf from './projects/DiffusionScarf'
@@ -35,7 +35,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/experimental",
-    element: <StretchLengths/>
+    element: <Experimental/>
   },
   {
     path: "/parallax",
