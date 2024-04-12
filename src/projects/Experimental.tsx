fabebf465012a35d033a05d5e44037bb6f71191b
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-04-12 11:32:29 -0700

    add yardage form to experimental page

--- src/projects/Experimental.tsx
+++ src/projects/Experimental.tsx
@@ -1,9 +1,11 @@
 import SwatchWithForm from '../SwatchWithForm';
-import { StitchPattern, Color } from '../types'
+import { StitchPattern, Color, SwatchConfig } from '../types'
 import { useSwatchConfigStateFromURLParams, useEffectToUpdateURLParamsFromSwatchConfig } from '../URLSwatchParams';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ColorSequenceInfo from '../ColorSequenceInfo'
 import DropdownInput from '../inputs/Dropdown';
+import CheckboxInput from '../inputs/Checkbox';
+import NumericInput from '../inputs/Numeric';
 import { totalColorSequenceLength } from '../color'
 
 const red = "#ff001d" as Color;
@@ -12,6 +14,75 @@ const ltblue = "#8dd0f2" as Color;
 const navy = "#0e0e66" as Color;
 
 type StaggerType = 'colorStretched' | 'colorSwallowed'
+
+function YardageForm(
+  { swatchConfig, setSwatchConfig, active } : 
+  {
+    swatchConfig: SwatchConfig,
+    setSwatchConfig: (data: SwatchConfig) => void,
+    active: boolean,
+  }
+) {
+  const [numberOfSkeins, setNumberOfSkeins] = useState(1 as number | undefined)
+  const [yardage, setYardage] = useState(230 as number | undefined)
+  const [colorSequenceInches, setColorSequenceInches] = useState(64 as number | undefined)
+
+  useEffect(() => {
+    if(active && numberOfSkeins && yardage && colorSequenceInches) {
+      const totalInchesOfYarn = yardage * numberOfSkeins * 3 * 12;
+      const possibleColorRepeatsFloat = totalInchesOfYarn/colorSequenceInches;
+      const stitchesPerSequence = totalColorSequenceLength(swatchConfig.colorSequence);
+      const maxStitchesToShow = Math.floor(stitchesPerSequence * possibleColorRepeatsFloat);
+      const rowsToShow = Math.floor(maxStitchesToShow/swatchConfig.stitchesPerRow)
+      if(rowsToShow !== swatchConfig.numberOfRows) {
+        setSwatchConfig({...swatchConfig, numberOfRows: rowsToShow})
+      }
+    }
+  }, [active, numberOfSkeins, yardage, colorSequenceInches, swatchConfig, setSwatchConfig])
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <fieldset>
+        <CheckboxInput
+          label="Imperial Units"
+          title="Use yards and inches (sorry metric friends this box is always checked)"
+          name="units"
+          withTooltip={true}
+          value={true}
+          setValue={() => true}
+        />
+        <NumericInput
+          label="Yarn yardage:"
+          title="Yardage of one skien of yarn"
+          name="yardage"
+          value={yardage}
+          setValue={setYardage}
+          validator={NumericInput.validators.nonNegative}
+        />
+        <NumericInput
+          label="Length of one color sequence (in):"
+          title="This is twice the skein length for a hand dyed skein of yarn"
+          withTooltip={true}
+          name="colorSequenceInches"
+          value={colorSequenceInches}
+          setValue={setColorSequenceInches}
+          validator={NumericInput.validators.nonNegative}
+        />
+        <NumericInput
+          label="Number of skeins:"
+          title="Number of balls of yarn with your yardage above"
+          name="numberOfSkeins"
+          value={numberOfSkeins}
+          setValue={setNumberOfSkeins}
+          validator={NumericInput.validators.positive}
+        />
+      </fieldset>
+    </form>
+  )
+}
 
 function Experimental() {
   const defaultSwatchConfig = {
@@ -33,6 +104,7 @@ function Experimental() {
   const { swatchConfig, setSwatchConfig, setSearchParams} = useSwatchConfigStateFromURLParams(defaultSwatchConfig);
 
   const [staggerType, setStaggerType] = useState('colorStretched' as StaggerType)
+  const [displayYardageForm, setDisplayYardageForm] = useState(false)
 
   const setStaggerTypeFromDropdown = (newStaggerType: string) => {
     setStaggerType(newStaggerType as StaggerType)
@@ -48,16 +120,16 @@ function Experimental() {
       <p>This is an experimental page. It might change at anytime and URLs can break.</p>
       <p>Here are the current features:</p>
       <ul>
-        <li>there is a button to double your colors that makes a copy of all the colors</li>
-        <li>there's more info about the colors you see</li>
+        <li>there is a button to double your colors that makes a copy of all the colors (does not work well with yardage form)</li>
+        <li>there&apos;s more info about the colors you see</li>
         <li>you can change stagger type (not saved on url)</li>
+        <li>you can change the number of rows from the yardage form based on the measured color sequence length and yarn yardage</li>
       </ul>
       <p>If you don&apos;t know what I&apos;m talking about use the <a href='/'>main app.</a></p>
       <form
         onSubmit={(e) => {
           e.preventDefault();
         }}
-        className='wide-first-column'
       >
         <fieldset>
           <ColorSequenceInfo colorSequence={swatchConfig.colorSequence} colorShift={swatchConfig.colorShift}/>
@@ -76,8 +148,16 @@ function Experimental() {
               {label: 'Color swallowing (loosening tension)', value: 'colorSwallowed'},
             ]}
           />
+          <CheckboxInput
+            title="Show a form to change the length of the preview based on yarn yardage, number of skeins, and color sequence length in inches"
+            label="Use yarn yardage form instead of number of rows"
+            name="displayYardageForm"
+            value={displayYardageForm}
+            setValue={setDisplayYardageForm}
+          />
         </fieldset>
       </form>
+      <YardageForm swatchConfig={swatchConfig} setSwatchConfig={setSwatchConfig} active={displayYardageForm}/>
       <SwatchWithForm
         swatchConfig={swatchConfig}
         setSwatchConfig={setSwatchConfig}
