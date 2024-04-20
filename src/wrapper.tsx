28bbd6e9f6f283acd212cd603aa7cb694992f83b
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-04-19 19:28:31 -0700

    add entrelac page

--- src/wrapper.tsx
+++ src/wrapper.tsx
@@ -7,6 +7,7 @@ import Sunflower from './projects/Sunflower'
 import Doodle from './projects/Doodle'
 import LogoOption from './projects/LogoOption'
 import Experimental from './projects/Experimental'
+import Entrelac from './projects/Entrelac'
 import Parallax from './projects/Parallax'
 import DoloresParkTote from './projects/DoloresParkTote'
 import SwimLesson from './projects/SwimLesson'
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
   {
     path: "/experimental",
     element: <Experimental/>
+  },
+  {
+    path: "/entrelac",
+    element: <Entrelac/>
   },
   {
     path: "/parallax",
