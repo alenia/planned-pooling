d0c2c10bf40a495bd9ec459967892f7782d7e7d8
Daniela Wellisz <daniela.wellisz@gmail.com>
2024-05-17 13:58:03 -0700

    design spike

--- src/projects/LogoOption.tsx
+++ src/projects/LogoOption.tsx
@@ -1,7 +1,7 @@
 //This file is a copy of App.jsx but with my specific color config for what I'm working on and advanced stitch patterns
 //Eventually I just want to load the data from specific JSON for my personal projects
 //But for now I'll do this
-import SwatchWithForm from '../SwatchWithForm';
+import Swatch from '../Swatch';
 import { StitchPattern, Color } from '../types'
 import { useState } from "react";
 
@@ -11,23 +11,43 @@ const cream = "#fefbec" as Color;
 const lightTeal = "#70afb7" as Color;
 
 function LogoOption() {
-  const [swatchConfig, setSwatchConfig] = useState({
-    colorSequence: [
-      {color: coral, length: 2},
-      {color: teal, length: 5},
-      {color: coral, length: 2},
-      {color: cream, length: 4},
-      {color: lightTeal, length: 0},
-    ],
-    stitchesPerRow: 19,
-    numberOfRows: 40,
+  const sharedConfig = {
+    stitchesPerRow: 16,
+    stitchPattern: StitchPattern.moss,
+    className: 'no-margin'
+  }
+  const singleSequence = [
+    {color: lightTeal, length: 8},
+    {color: teal, length: 8},
+  ]
+  const alternatingSequence = [
+    {color: teal, length: 6},
+    {color: lightTeal, length: 10},
+    {color: teal, length: 6},
+    {color: lightTeal, length: 11},
+  ]
+  const stripedConfig = {
+    colorSequence: singleSequence,
+    numberOfRows: 38,
     colorShift: 0,
     staggerLengths: false,
-    stitchPattern: StitchPattern.moss,
-  })
+    ...sharedConfig
+  }
 
+      //<Swatch staggerType='colorSwallowed' {...stripedConfig} staggerLengths={true} numberOfRows={16} colorShift={9}/>
   return (
-    <SwatchWithForm swatchConfig={swatchConfig} setSwatchConfig={setSwatchConfig} />
+    <div>
+      <div style={{transformOrigin: '300px 300px', transform: 'rotate(270deg)', height: 'fit-content', width: 'fit-content'}}>
+        <Swatch staggerType='normal' {...stripedConfig}/>
+        <Swatch staggerType='colorStretched' {...stripedConfig} staggerLengths={true} numberOfRows={20} colorShift={9}/>
+        <Swatch staggerType='colorStretched' {...stripedConfig} staggerLengths={true} numberOfRows={48}/>
+      </div>
+      <div style={{transformOrigin: '300px 300px', transform: 'rotate(270deg)', height: 'fit-content', width: 'fit-content'}}>
+        <Swatch staggerType='normal' {...stripedConfig}/>
+        <Swatch staggerType='colorStretched' {...stripedConfig} staggerLengths={true} numberOfRows={64}/>
+        <Swatch staggerType='normal' {...stripedConfig}/>
+      </div>
+    </div>
   );
 }
 
