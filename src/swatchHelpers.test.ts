e83b4ef6c6485b77eb526d4af175db01d4e3ac7f
Daniela Wellisz <daniela.wellisz@gmail.com>
2025-01-17 15:13:05 -0800

    add normal staggering and color stretching logic to swatchMatrix

--- src/swatchHelpers.test.ts
+++ src/swatchHelpers.test.ts
@@ -18,7 +18,8 @@ describe('swatchMatrix', () => {
         stitchesPerRow: 4,
         numberOfRows: 3,
         staggerLengths: false,
-        colorShift: 0
+        colorShift: 0,
+        staggerType: 'normal'
       })).toEqual([
         ["#aaa","#bbb","#ccc","#ddd"],
         ["#ccc","#bbb","#aaa","#eee"],
@@ -35,7 +36,8 @@ describe('swatchMatrix', () => {
       stitchesPerRow: 4,
       numberOfRows: 4,
       staggerLengths: false,
-      colorShift: 0
+      colorShift: 0,
+      staggerType: 'normal'
     })).toEqual([
       ["#000","#000","#000","#111"],
       ["#222","#222","#222","#111"],
@@ -55,7 +57,8 @@ describe('swatchMatrix', () => {
         stitchesPerRow: 5,
         numberOfRows: 3,
         staggerLengths: false,
-        colorShift: 0
+        colorShift: 0,
+        staggerType: 'normal'
       })).toEqual([
         ["#aaa","#bbb","#ccc","#ddd","#eee"],
         ["#eee","#ddd","#ccc","#bbb","#aaa"],
@@ -74,14 +77,74 @@ describe('swatchMatrix', () => {
         stitchesPerRow: 4,
         numberOfRows: 3,
         staggerLengths: false,
-        colorShift: 3
+        colorShift: 3,
+        staggerType: 'normal'
       })).toEqual([
         ["#ddd","#eee","#aaa","#bbb"],
         ["#aaa","#eee","#ddd","#ccc"],
         ["#bbb","#ccc","#ddd","#eee"]
       ])
   })
-  it('staggers lengths')
-  it('swallows stitches')
-  it('stretches stitches')
+  it('makes odd rows have one extra color when staggerLengths is true and staggerType is normal', () => {
+    expect(
+      swatchMatrix({colorSequence: [
+        {color: '#aaa', length: 1},
+        {color: '#bbb', length: 1},
+        {color: '#ccc', length: 1},
+        {color: '#ddd', length: 1},
+        {color: '#eee', length: 1},
+      ] as ColorSequenceArray,
+        stitchesPerRow: 4,
+        numberOfRows: 3,
+        colorShift: 0,
+        staggerLengths: true,
+        staggerType: 'normal'
+      })).toEqual([
+        ["#aaa","#bbb","#ccc","#ddd","#eee"],
+        ["#ddd","#ccc","#bbb","#aaa"],
+        ["#eee","#aaa","#bbb","#ccc","#ddd"]
+      ])
+  })
+  it.skip('swallows stitches', () => { //NOTE: this is not handled in the existing code, it's just dealt with by CSS (so gross!!!)
+    expect(
+      swatchMatrix({colorSequence: [
+        {color: '#aaa', length: 1},
+        {color: '#bbb', length: 1},
+        {color: '#ccc', length: 1},
+        {color: '#ddd', length: 1},
+        {color: '#eee', length: 1},
+      ] as ColorSequenceArray,
+        stitchesPerRow: 4,
+        numberOfRows: 3,
+        colorShift: 0,
+        staggerLengths: true,
+        staggerType: 'colorSwallowed'
+      })).toEqual([
+        ["#aaa","#bbb","#ccc","#ddd"],
+        ["#ddd","#ccc","#bbb","#aaa"],
+        ["#eee","#aaa","#bbb","#ccc"]
+      ])
+  })
+  it('stretches stitches', () => {
+    expect(
+      swatchMatrix({colorSequence: [
+        {color: '#aaa', length: 1},
+        {color: '#bbb', length: 1},
+        {color: '#ccc', length: 1},
+        {color: '#ddd', length: 1},
+        {color: '#eee', length: 1},
+      ] as ColorSequenceArray,
+        stitchesPerRow: 4,
+        numberOfRows: 5,
+        colorShift: 0,
+        staggerLengths: true,
+        staggerType: 'colorStretched'
+      })).toEqual([
+        ["#aaa","#bbb","#ccc","#ddd"],
+        ["#ccc","#bbb","#aaa","#eee"],
+        ["#ccc","#ddd","#eee","#aaa"],
+        ["#eee","#ddd","#ccc","#bbb"],
+        ["#eee","#aaa","#bbb","#ccc"],
+      ])
+  })
 })
--- src/swatchHelpers.ts
+++ src/swatchHelpers.ts
@@ -2,14 +2,29 @@ import { StandardSwatchConfig, Color } from './types'
 import { flatColorSequenceArray } from './colorSequenceHelpers'
 import { circularSlice } from './arrayHelpers'
 
-export function swatchMatrix({ colorSequence, stitchesPerRow, numberOfRows, colorShift } : StandardSwatchConfig) : Array<Array<Color>>{
+export function swatchMatrix({
+  colorSequence,
+  stitchesPerRow,
+  numberOfRows,
+  colorShift,
+  staggerLengths,
+  staggerType
+} : StandardSwatchConfig & {staggerType: 'normal' | 'colorStretched' | 'colorSwallowed'}) : Array<Array<Color>>{
   const flattenedColorSequence = flatColorSequenceArray(colorSequence)
   const output = [] as Array<Array<Color>>
   let startingIndex = colorShift;
   for(let i = 0; i < numberOfRows; i++) {
-    const nextSlice = circularSlice(flattenedColorSequence, startingIndex, stitchesPerRow) as Array<Color>
+    const stitchesInThisRow = (staggerLengths && staggerType === 'normal' && i % 2 === 0) ? stitchesPerRow + 1 : stitchesPerRow;
+
+    const nextSlice = circularSlice(flattenedColorSequence, startingIndex, stitchesInThisRow) as Array<Color>
+
     i % 2 === 0 ? output.push(nextSlice) : output.push(nextSlice.reverse())
-    startingIndex += stitchesPerRow
+
+    if(staggerLengths && staggerType === 'colorStretched' && i % 2 === 1) {
+      startingIndex += stitchesInThisRow - 1
+    } else {
+      startingIndex += stitchesInThisRow
+    }
   }
   return output
 }
