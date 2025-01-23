52882e90bfb5ae1e613f66c7b5d60316a45d37d6
Daniela Wellisz <daniela.wellisz@gmail.com>
2025-01-22 16:14:25 -0800

    fix merge oversight

--- src/swatchHelpers.test.ts
+++ src/swatchHelpers.test.ts
@@ -2,7 +2,6 @@ import { describe, it, expect } from 'vitest'
 import { ColorSequenceArray } from './types'
 import {
   swatchMatrix,
-  swatchMatrixWithReversedEvenRows,
   clusteredSwatchMatrix,
 } from './swatchHelpers'
 
